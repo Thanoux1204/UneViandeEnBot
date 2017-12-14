fa83dfbe3b6ab84eef6d91a2add03ddb7a62b0a0
Thanoux1204 <34548741+Thanoux1204@users.noreply.github.com>
2017-12-14 18:59:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 ﻿const Discord = require('discord.js');
 const bot = new Discord.Client();
-const Google = require('./google')
 
 var prefix = (",")
 
@@ -41,7 +40,5 @@ bot.on('message', message => {
     if(message.content === prefix+ "stats"){
         message.channel.sendMessage("Il y a actuellement ${message.guild.channels.size} channels sur ce discord \n Il y a actuellement ${message.guilds.members.size} joueurs sur ce discord")
     }
-    if(Google.match(message)){
-        return Google.action(message)
-    }
+
 });
